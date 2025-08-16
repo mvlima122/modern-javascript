5a614559b94e900eecb0b4ae35cfccd15da44e72
Maria Vitoria <vivilima71@gmail.com>
2025-08-16 10:45:57 -0300

    trabalhando com arrow function

---
+++ scripts/arrow-functions.js
@@ -0,0 +1,17 @@
+
+// forma tradicional de uma arrow function
+// const saudar = () => {
+//     console.log("Olá!");
+// };
+
+// saudar();
+
+const multiplicar = (numero) => {
+    return numero * 2;
+}
+
+const resultado = multiplicar(10);
+console.log(resultado)
+
+// forma curta e concisa de uma arrow function
+// const multiplicar = numero => numero * 2;
